41ee50d31b35f9f194283649c185dffb8c81f323
Gustavo Cassel <gustavo.cassel.52@gmail.com>
2024-08-18 21:35:29 -0300

    feat: login and register in same page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
-import { navigateByLoginState, auth } from "./context/FirebaseContext";
+import { navigateByLoginState } from "./context/FirebaseContext";
 import { CREATE_SUPPLIER_ENDPOINT, EDIT_SUPPLIER_ENDPOINT, HOME_ENDPOINT, LOGIN_ENDPOINT, NOT_FOUND_ENDPOINT, SUPPLIERS_ENDPOINT } from "./data/constants";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import LoginRegister from "./pages/LoginRegister";
 import Navbar from "./pages/Navbar";
 import NotFound from "./pages/NotFound";
+
 import Suppliers from "./pages/Suppliers";
 import CreateSupplier from "./pages/Suppliers/CreateSupplier";
 import EditSupplier from "./pages/Suppliers/EditSupplier";
@@ -20,15 +21,11 @@ function App() {
     navigateByLoginState(navigate);
   }, [navigate]);
 
-  auth.onAuthStateChanged(() => {
-    navigateByLoginState(navigate);
-  });
-
   return (
     <Routes>
       <Route path={HOME_ENDPOINT} element={<Navbar />}>
         <Route index element={<Home />} />
-        <Route path={LOGIN_ENDPOINT} element={<Login />} />
+        <Route path={LOGIN_ENDPOINT} element={<LoginRegister />} />
         <Route path={NOT_FOUND_ENDPOINT} element={<NotFound />} />
         <Route path={SUPPLIERS_ENDPOINT} element={<Suppliers />} />
         <Route path={CREATE_SUPPLIER_ENDPOINT} element={<CreateSupplier />} />
--- src/pages/LoginRegister.tsx
+++ src/pages/LoginRegister.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FirebaseError } from "firebase/app";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { Button, Col, Container, FloatingLabel, Form, Row } from "react-bootstrap";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -10,29 +10,46 @@ import { auth, getFirebaseErrorDescription } from "../context/FirebaseContext";
 import { HOME_ENDPOINT } from "../data/constants";
 import { Toast } from "../utils/Alerts";
 
-const schema = z.object({
-  email: z.string().email({ message: "Email inválido" }),
-  password: z.string().min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
-});
+const schema = z
+  .object({
+    email: z.string().email({ message: "Email inválido" }),
+    password: z.string().min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
+    confirmPassword: z.string().min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["password", "confirmPassword"],
+    message: "As senhas não coincidem",
+  });
 
-type LoginData = z.infer<typeof schema>;
+type FormData = z.infer<typeof schema>;
 
-export default function Login() {
+export default function Auth() {
   const navigate = useNavigate();
+  const [isLoginMode, setIsLoginMode] = useState(true);
   const [buttonVisible, setButtonVisible] = useState(true);
 
   const {
     register,
     handleSubmit,
     setError,
+    reset,
     formState: { errors, isSubmitting },
-  } = useForm<LoginData>({
+  } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
-  const onSubmit: SubmitHandler<LoginData> = async ({ email, password }) => {
+  const onSubmit: SubmitHandler<FormData> = async ({ email, password }) => {
     try {
-      await signInWithEmailAndPassword(auth, email, password);
+      if (isLoginMode) {
+        await signInWithEmailAndPassword(auth, email, password);
+      } else {
+        await createUserWithEmailAndPassword(auth, email, password);
+        Toast.fire({
+          icon: "success",
+          title: "Registro realizado com sucesso",
+          text: "Você será redirecionado para a página inicial.",
+        });
+      }
     } catch (error) {
       if (error instanceof FirebaseError) {
         const firebaseError = error as FirebaseError;
@@ -43,7 +60,7 @@ export default function Login() {
       }
 
       setError("root", {
-        message: "Erro ao realizar login",
+        message: isLoginMode ? "Erro ao realizar login" : "Erro ao realizar registro",
       });
       return;
     }
@@ -52,7 +69,7 @@ export default function Login() {
 
     Toast.fire({
       icon: "success",
-      title: "Login realizado com sucesso",
+      title: isLoginMode ? "Login realizado com sucesso" : "Registro realizado com sucesso",
       text: "Redirecionando para a página inicial.",
     });
 
@@ -61,11 +78,16 @@ export default function Login() {
     }, 1000);
   };
 
+  const toggleMode = () => {
+    setIsLoginMode((prevMode) => !prevMode);
+    reset();
+  };
+
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col md="4">
-          <h2 className="text-center mb-3">Login</h2>
+          <h2 className="text-center mb-3">{isLoginMode ? "Login" : "Registrar"}</h2>
           <Form>
             <FloatingLabel controlId="floatingInput" label="Digite seu email" className="mb-3">
               <Form.Control type="email" placeholder="" isInvalid={!!errors.email} {...register("email")} />
@@ -77,14 +99,24 @@ export default function Login() {
               <Form.Control.Feedback type="invalid">{errors.password?.message}</Form.Control.Feedback>
             </FloatingLabel>
 
+            {!isLoginMode && (
+              <FloatingLabel controlId="floatingConfirmPassword" label="Confirme sua senha" className="mb-3">
+                <Form.Control type="password" placeholder="" isInvalid={!!errors.confirmPassword} {...register("confirmPassword")} />
+                <Form.Control.Feedback type="invalid">{errors.confirmPassword?.message}</Form.Control.Feedback>
+              </FloatingLabel>
+            )}
+
             {buttonVisible && (
               <>
                 <Button variant="primary" type="submit" className="w-100" disabled={isSubmitting} onClick={handleSubmit(onSubmit)}>
-                  {isSubmitting ? "Entrando..." : "Entrar"}
+                  {isSubmitting ? (isLoginMode ? "Entrando..." : "Registrando...") : isLoginMode ? "Entrar" : "Registrar"}
                 </Button>
                 <Form.Control.Feedback type="invalid" className="d-block mt-2">
                   {errors.root?.message}
                 </Form.Control.Feedback>
+                <Button variant="link" className="w-100 mt-3" onClick={toggleMode}>
+                  {isLoginMode ? "Criar uma conta" : "Já tem uma conta? Entre aqui"}
+                </Button>
               </>
             )}
           </Form>
